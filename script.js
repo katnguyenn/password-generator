0a212f880a4dde86bd981ee9d938bf0f154da3a8
Katherine Nguyen <nguyenkat01@yahoo.com>
2020-10-22 10:48:42 -0700

    Created function to generate desired number of characters in password

--- script.js
+++ script.js
@@ -16,14 +16,27 @@ generateBtn.addEventListener("click", writePassword);
 // Variable Declarations
 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 var passwordLength;
 var passwordNumber;
+var passwordChar
 var passwordLowerCase;
 var passwordUpperCase;
 
-function writePassword()
+// Function to generate desired number of characters in password
+
+function writePassword() {
   var passwordLength = prompt("How many characters do you want your password to have?");
-  
+      if (passwordLength <= 7 || passwordLength >= 129) {
+        alert("Your password should have between 8-128 characters. Please try again.");
+    var passwordLength = prompt("How many characters do you want your password to have?");
+      } else {
+        alert("Great! Just a few more questions before we generate your secure password.");
+      }
+
+    }
+
+
