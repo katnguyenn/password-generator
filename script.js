b536810262534d12bc2f388b1da4ac6da82504d5
Katherine Nguyen <nguyenkat01@yahoo.com>
2020-10-21 19:20:02 -0700

    Declared variables for numbers, letters and prompt.

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Variable Declarations
+
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var passwordLength;
+var passwordNumber;
+var passwordLowerCase;
+var passwordUpperCase;
+
+function writePassword()
+  var passwordLength = prompt("How many characters do you want your password to have?");
+  
